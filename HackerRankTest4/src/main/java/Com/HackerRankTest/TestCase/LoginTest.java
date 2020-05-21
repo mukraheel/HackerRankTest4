681db8d6b3609488bd8b024d3d150a81b52cf479
Raheel <Raheel@DESKTOP-U6EFAKA>
2020-05-21 20:40:54 +0500

    ExcelSheet added

--- HackerRankTest4/src/main/java/Com/HackerRankTest/TestCase/LoginTest.java
+++ HackerRankTest4/src/main/java/Com/HackerRankTest/TestCase/LoginTest.java
@@ -6,6 +6,9 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Properties;
 
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -50,6 +53,7 @@ public class LoginTest extends BaseClass{
 
 	}
 	
+	
 	@Test
 	public void CreateAccountTest() throws IOException
 	{
@@ -65,7 +69,7 @@ public class LoginTest extends BaseClass{
 		
 		lp.loginlink();
 		lp.Reglink();
-		//lp.setEmailaddress(DataProviderFactory.getRandomDataProperty().getValue("Emailaddress"));
+		lp.setEmailaddress(DataProviderFactory.getRandomDataProperty().getValue("Emailaddress"));
 		lp.setFirstname(DataProviderFactory.getRandomDataProperty().getValue("Firstname"));
 		lp.setPasswoard(DataProviderFactory.getRandomDataProperty().getValue("Password"));
 		lp.clickBtn();
@@ -76,21 +80,84 @@ public class LoginTest extends BaseClass{
 		if(driver.findElement(By.id("aria-join_neu_email_field-error")).getText().equals("Can't be blank."))
 		{
 			Assert.assertTrue(true);
-			System.out.println("inside true condition");
+			System.out.println("inside Emailaddress condition");
 		}
-		else
+		else if(driver.findElement(By.id("aria-join_neu_password_field-error")).getText().equals("Can't be blank."))
 		{
-			Assert.assertTrue(false);
-			System.out.println("inside false condition");
+			Assert.assertTrue(true);
+			System.out.println("inside Firstname condition");
 		}
+		
 			
 	}
 	
-	@Test 
-	public void requiredFilds()
-	{
-		 driver.findElement(By.id("aria-join_neu_email_field-error")).getText();
-		
+	
+	public void ReadExcelData() throws IOException {
+		try {
+
+			String ExcelData = null;
+
+			File src = new File("configs/SalesforceData.xlsx");
+
+			// load file
+			FileInputStream fis = new FileInputStream(src);
+
+			// Load workbook
+			XSSFWorkbook wb = new XSSFWorkbook(fis);
+
+			// Load sheet- Here we are loading first sheetonly
+			XSSFSheet sh1 = wb.getSheetAt(0);
+
+			// Find number of rows in excel file
+
+			int rowCount = sh1.getLastRowNum() - sh1.getFirstRowNum();
+			
+			System.out.println("rowsCount :" + rowCount);
+			// Create a loop over all the rows of excel file to read it
+
+			for (int i = 1; i < rowCount + 1; i++) {
+
+				Row row = sh1.getRow(i);
+				
+				// Create a loop to print cell values in a row
+				
+				if(row==null){
+                    sh1.getRow(i+1);
+                     continue;
+                 }
+				
+				for (int j = 0; j < row.getLastCellNum(); j++) {
+					
+					// Print Excel data in console
+					ExcelData = row.getCell(j).getStringCellValue();
+					
+
+					if (row.getCell(j) != null && (row.getCell(j).toString().equalsIgnoreCase(ExcelData))) {
+						sh1.removeRow(row);
+						//sh1.shiftRows( 4, sh1.getLastRowNum(), -1);
+						
+						System.out.println("Data Remove :" + ExcelData);
+						// sheet.shiftRows(i, lastIndex, -1);
+						
+						
+					}
+				}
+				break;
+			}
+			
+			FileOutputStream fileOut = new FileOutputStream(src);
+			wb.write(fileOut);
+			fileOut.close();
+
+		} catch (
+
+		Exception e) {
+
+			System.out.println(e.getMessage());
+
+		}
 	}
+	
+	
 
 }
